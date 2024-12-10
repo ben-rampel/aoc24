73e154013e24c7fc0fcb06446decc1957ad3efcf
Ben Rampel <brampel23@gmail.com>
2024-12-10 18:14:08 -0500

    day 5 pt 2

--- aoc_day_5.zig
+++ aoc_day_5.zig
@@ -12,6 +12,16 @@ const NumStatus = enum {
     dependent,
 };
 
+fn lessThanByRules(dependencies: [100]ArrayList(u8), a: u8, b: u8) bool {
+    if (a == b) return false;
+    for (dependencies[a].items) |dep| {
+        if (dep == b) {
+            return false;
+        }
+    }
+    return true;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
@@ -35,19 +45,19 @@ pub fn main() !void {
     }
 
     var middleSum: u32 = 0;
+    var invalidMiddleSum: u32 = 0;
     while (lines.next()) |line| {
         if (line.len == 0) break;
         print("{s}\n", line);
         var nums = std.mem.splitScalar(u8, line, ',');
         var seen = std.mem.zeroes([100]NumStatus);
         var validLine = true;
-        var numList = ArrayList(u32).init(alloc);
+        var numList = ArrayList(u8).init(alloc);
         while (nums.next()) |numStr| {
-            const numParsed = try std.fmt.parseUnsigned(u32, numStr, 10);
+            const numParsed = try std.fmt.parseUnsigned(u8, numStr, 10);
             try numList.append(numParsed);
             if (seen[numParsed] == NumStatus.dependent) {
                 validLine = false;
-                break;
             }
             for (dependencies[numParsed].items) |dep| {
                 if (seen[dep] != NumStatus.seen) seen[dep] = NumStatus.dependent;
@@ -56,7 +66,12 @@ pub fn main() !void {
         }
         if (validLine) {
             middleSum += numList.items[numList.items.len / 2];
+        } else {
+            std.mem.sort(u8, numList.items, dependencies, lessThanByRules);
+            invalidMiddleSum += numList.items[numList.items.len / 2];
+            print("sort: {any}\n", numList.items);
         }
     }
-    print("{}\n", middleSum);
+    print("Middle sum: {}\n", middleSum);
+    print("Invalid middle sum: {}\n", invalidMiddleSum);
 }
